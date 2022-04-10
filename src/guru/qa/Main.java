f9429fc31e138289028d5d3d0ae7ba5b1c6bb6f9
tdv14011984@gmail.com <Parol2022!-forGIT>
2022-04-10 20:10:53 +0300

    add new lines

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -5,6 +5,16 @@ public class Main {
     public static void main(String[] args) {
         // домашнее задание 4. Основы Java. Дмитрий Тучс. от 05 апреля 2022 г.
 
+        char symbol, anotherSymbol;
+        symbol = 'w';
+        boolean flag = false;
+        byte aByte = 1;
+        short aShort;
+        int aInt = 10;
+        Integer boxed = null;
+        long aLong = 123L;
+        float aFloat = 1.2F;
+        double aDouble = 1.2D;
 
     }
 }
